1dde806b8dc1b93e3e40bfa8cb390199ff340770
hcaatu <aatu.hokkane@gmail.com>
2024-09-26 13:51:01 +0300

    3.8 done

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express')
+var morgan = require('morgan')
 const app = express()
+
+
+app.use(express.json())
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+morgan(':method :url :status :res[content-length] - :response-time ms :body')
 
 let persons = [
   {
@@ -41,6 +47,52 @@ app.get('/info', (request, response) => {
        ${now.toString()}`
       )
   }
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+})
+
+const randomInt = () => {
+  return Math.floor(Math.random() * 10000)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log(body)
+  
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'missing name or number'
+    })
+  }
+
+  if (persons.map(person => person.name).includes(body.name)) {
+    return response.status(400).json({
+      error: 'name already exists'
+    })
+  }
+
+  const person = {
+    id: randomInt(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+  response.json(person)
 })
 
 const PORT = 3001

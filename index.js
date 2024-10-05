be26f4a6e25320428c33d9f55e1d69e88f9efdba
hcaatu <aatu.hokkane@gmail.com>
2024-10-05 13:25:51 +0300

    uibuild

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-require('dotenv').config()
 const express = require('express')
-const morgan = require('morgan')
 const app = express()
-const cors = require('cors')
+require('dotenv').config()
+
 const Person = require('./models/person')
 
-app.use(express.json())
 app.use(express.static('dist'))
-app.use(cors())
+
+const morgan = require('morgan')
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
-morgan(':method :url :status :res[content-length] - :response-time ms :body')
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({error: 'id in wrong format'})
+  }
+
+  next(error)
+}
+
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({error: 'unknown endpoint'})
+}
 
 app.get('/info', (request, response, next) => {
   Person.find({}).then(people => {
@@ -37,21 +55,20 @@ app.get('/api/persons', (request, response, next) => {
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-  Person.findById(request.params.id).then(person => {
+  Person.findById(request.params.id)
+  .then(person => {
     if (person) {
       response.json(person)
     } else {
       response.status(404).end()
     }
   })
-  .catch(error => {
-    console.log(error)
-    next(error)
-  })
+  .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-  Person.findByIdAndDelete(request.params.id).then(person => {
+  Person.findByIdAndDelete(request.params.id)
+  .then(person => {
     response.status(204).end()
   })
   .catch(error => next(error))
@@ -63,7 +80,6 @@ const randomInt = () => {
 
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
-  console.log(body)
   
   if (!body.name || !body.number) {
     return response.status(400).json({
@@ -73,7 +89,7 @@ app.post('/api/persons', (request, response, next) => {
 
   // check if name exists already
   Person.find({}).then(people => {
-
+    
     const person = new Person({
       name: body.name,
       number: body.number,
@@ -89,6 +105,7 @@ app.post('/api/persons', (request, response, next) => {
     person.save().then(savedPerson => {
       response.json(savedPerson)
     })
+    .catch(error => next(error))
 
   })
   .catch(error => next(error))
@@ -109,6 +126,9 @@ app.put('/api/persons/:id', (req, res, next) => {
     })
     .catch(error => next(error))
 })
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {

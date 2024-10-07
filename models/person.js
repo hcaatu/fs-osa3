e54937993a5e4118d6aac707bd4f77161a9bb91f
hcaatu <aatu.hokkane@gmail.com>
2024-10-07 14:05:12 +0300

    lint fixes

--- models/person.js
+++ models/person.js
@@ -5,40 +5,40 @@ const url = process.env.MONGODB_URI
 mongoose.set('strictQuery', false)
 console.log('connecting to', url)
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB', error.message)
-    })
-
-const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 3,
-        required: true
-    },
-    number: {
-        type: String,
-        minlength: 8,
-        validate: {
-            validator: function(v) {
-                const re = new RegExp(/(^\d{2}-\d{5,}$|^\d{3}-\d{4,}$)/)
-                return re.test(v)
-            },
-            message: props => `${props.value} is not a valid phone number!`
-        },
-        required: true
-    },
-    visible: Boolean,
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB', error.message)
   })
 
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    validate: {
+      validator: function(v) {
+        const re = new RegExp(/(^\d{2}-\d{5,}$|^\d{3}-\d{4,}$)/)
+        return re.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: true
+  },
+  visible: Boolean,
+})
+
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
